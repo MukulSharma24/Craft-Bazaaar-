8beaa80fd55aefc8dccc23bf59518e3b690907cb
Akshita mishra <mishraakshita916@gmail.com>
2026-09-19 02:40:22 +0530

    Update user API endpoint to use production URL

--- frontend/src/util/userApi.js
+++ frontend/src/util/userApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const fetchUser = async (userId) => {
   try {
-    const response = await axios.get(`/users/${userId}`);
+    const response = await axios.get(`https://craft-bazaaar.onrender.com/users/${userId}`);
     return response.data; // Assuming the API returns user details as an object
   } catch (error) {
     console.error('Error fetching user data:', error);
