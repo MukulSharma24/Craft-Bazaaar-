a74d3c2412f857070ee6547f57ab7ec2e8e33d8b
Akshita mishra <mishraakshita916@gmail.com>
2026-09-19 02:39:39 +0530

    Update cart API endpoint to production URL

--- frontend/src/util/cartApi.js
+++ frontend/src/util/cartApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const fetchCartItems = async (userId) => {
   try {
-    const response = await axios.get(`https://localhost:3389/cart/${userId}`);
+    const response = await axios.get(`https://craft-bazaaar.onrender.com/cart/${userId}`);
     return response.data;
   } catch (error) {
     throw new Error('Error fetching cart items:', error);
